db2d1117a58dee238fcc771d8fdcfee5f796d4ce
LuaraSouto <109703333+LuaraSouto@users.noreply.github.com>
2022-08-11 20:01:21 -0300

    Add files via upload

---
+++ migration.js
@@ -0,0 +1,14 @@
+import connection from "./config/db.js";
+import Restaurant from "./models/Restaurant.js";
+import Review from "./models/Review.js";
+
+const migrate = async () => {
+    try {
+        const result = await connection.sync();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+migrate();
